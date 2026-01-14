0c7b8172e13e1c8a461c5db2eb10e3b0bdb0ecbd
Borna <borna.colaric@gmail.com>
2026-01-14 22:24:29 +0100

    Initial solution

---
+++ DbcParser.App/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DbcParser.App;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DbcParser.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace DbcParser.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
