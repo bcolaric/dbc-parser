e0a442f9243f30f23f741e9234c87135ba065e73
Borna <borna.colaric@gmail.com>
2026-01-16 19:46:00 +0100

    Open DBC file from GUI

--- DbcParser.App/MainWindow.axaml.cs
+++ DbcParser.App/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DbcParser.App;
 
@@ -7,5 +11,31 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void OnLoadDbcClicked(object? sender, RoutedEventArgs e)
+    {
+        var options = new FilePickerOpenOptions
+        {
+            Title = "Open DBC file",
+            AllowMultiple = false,
+            FileTypeFilter =
+            [
+                new FilePickerFileType("DBC files")
+                {
+                    Patterns = ["*.dbc"]
+                }
+            ]
+        };
+
+        var files = await this.StorageProvider.OpenFilePickerAsync(options);
+
+        if (files.Count > 0)
+        {
+            await using var stream = await files[0].OpenReadAsync();
+            using var reader = new StreamReader(stream);
+
+            DbcContentTextBox.Text = await reader.ReadToEndAsync();
+        }
     }
 }
